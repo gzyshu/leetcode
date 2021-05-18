f2e683f8b2760fef1947430667265b47edfb9022
gzyshu <30323861+gzyshu@users.noreply.github.com>
2021-05-18 14:21:36 +0800

    打卡

--- 每日一题/day0518.java
+++ 每日一题/day0518.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
 
 public class day0518 {
+    public int countTriplets(int[] arr) {
+        if(arr.length<2) return 0;
+        int ans=0;
+        int len=arr.length;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=i+1;j<len;j++)
+            {
+                int sum1=0;
+                for(int k=i;k<=j;k++) sum1^=arr[k];
+                if(sum1==0) ans+=j-i;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
 }
+
